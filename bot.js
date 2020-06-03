7354b27aee544e74120ae50957f99fb1e75f9ed6
mnt4 <mnt4@users.noreply.github.com>
2020-06-03 23:47:48 +0300

    Project init + basic functions

---
+++ bot.js
@@ -0,0 +1,36 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '1165478309:AAG3eJBpBrVC3l8CvPjsERo-ISmxXyaE1kU';
+const bot = new TelegramBot(token, { polling: true });
+
+// Links array
+let scheduleLink = [
+    'https://i.imgur.com/5WyHjD8.jpeg', // 6Y8 [0]
+    '', // 7Y9 [1]
+    'imgur.com', // 8Y10 [2]
+    'imgur.com', // 9Y11 [3]
+    'https://i.imgur.com/Uvigbqn.jpg' // School breaks [4]
+];
+
+// Schedules
+bot.onText(/\/schedule (.+)/, (msg, match) => {
+    const chatID = msg.chat.id;
+    const scheduleYear = match[1];
+
+    if (scheduleYear == 6) {
+        bot.sendPhoto(chatID, scheduleLink[0], { caption: `Ваше расписание для ${scheduleYear} класса` });
+    } else if (scheduleYear == 7) {
+        bot.sendPhoto(chatID, scheduleLink[1], { caption: `Ваше расписание для ${scheduleYear} класса` });
+    } else if (scheduleYear == 8) {
+        bot.sendPhoto(chatID, scheduleLink[2], { caption: `Ваше расписание для ${scheduleYear} класса` });
+    } else if (scheduleYear == 9) {
+        bot.sendPhoto(chatID, scheduleLink[3], { caption: `Ваше расписание для ${scheduleYear} класса` });
+    } 
+});
+
+// School Calendar
+bot.onText(/\/breaks/, (msg) => {
+    const chatID = msg.chat.id;
+
+    bot.sendPhoto(chatID, scheduleLink[4], { caption: `Расписание каникул` });
+});
+  
